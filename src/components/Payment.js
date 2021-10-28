be0d623d6b9f06558ee5244cd9eb3b0df44b9f7f
Arnav Shekaran <arnard76@gmail.com>
2021-10-28 22:57:50 +1300

    payments list + payment component

--- src/components/Payment.js
+++ src/components/Payment.js
@@ -1,5 +1,28 @@
 import React from "react";
 
-export default function Payment() {
-  return <div>Payment#id</div>;
+export default function Payment(props) {
+  return (
+    <div className="flex justify-evenly p-2 my-4 bg-blue-50 rounded-xl border-blue-400 border-2">
+      <p className="flex-1">
+        ${props.value} {props.currency}
+      </p>
+      {/* <div style={{ width: "2px", backgroundColor: "black" }}></div> */}
+      <p className="flex-1">{props.name}</p>
+      <p className="flex-1">
+        {props.frequency[0] + ` times a ` + props.frequency[1]}
+      </p>
+    </div>
+  );
+}
+
+export function Payment2(props) {
+  return (
+    <div className="grid justify-evenly grid-cols-3 ">
+      <p>
+        ${props.value} {props.currency}
+      </p>
+      <p>{props.name}</p>
+      <p>{props.frequency[0] + ` times a ` + props.frequency[1]}</p>
+    </div>
+  );
 }
--- src/components/Payments.js
+++ src/components/Payments.js
@@ -1,10 +1,34 @@
 import React from "react";
-import Payment from "./Payment";
+import Payment, { Payment2 } from "./Payment";
 
 export default function Payments() {
   return (
-    <div>
-      <Payment />
+    <div className="flex-1 p-4 h-screen overflow-y-auto">
+      <strong className=" text-xl text-center my-2">
+        <h1>What does your financial goal look like?</h1>
+      </strong>
+      <Payment name="Haircut" value={25} frequency={[1, "Month"]} />
+      <Payment name="Groceries" value={200} frequency={[4, "Month"]} />
+      <Payment
+        name="New Very Long Payment"
+        value={200000}
+        frequency={[2, "Day"]}
+      />
+      <Payment name="Haircut" value={25} frequency={[1, "Month"]} />
+      <Payment name="Groceries" value={200} frequency={[4, "Month"]} />
+      <Payment
+        name="New Very Long Payment"
+        value={200000}
+        frequency={[2, "Day"]}
+      />
+
+      <Payment2 name="Haircut" value={25} frequency={[1, "Month"]} />
+      <Payment2 name="Groceries" value={200} frequency={[4, "Month"]} />
+      <Payment2
+        name="New Very Long Payment"
+        value={200000}
+        frequency={[2, "Day"]}
+      />
     </div>
   );
 }
