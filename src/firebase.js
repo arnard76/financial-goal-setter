b0c625807379e454bd88b9b0bea27a353e8a3637
Arnav Shekaran <arnard76@gmail.com>
2021-11-13 14:05:01 +1300

    Create GitHub action for Firebase

--- src/firebase.js
+++ src/firebase.js
@@ -8,7 +8,6 @@ const app = initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
