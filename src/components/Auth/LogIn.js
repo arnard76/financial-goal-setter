982192f739085ac639bfcb57737f798e5adf31a2
Arnav Shekaran <arnard76@gmail.com>
2021-12-08 14:47:14 +1300

    guest login functionality

--- src/components/Auth/LogIn.js
+++ src/components/Auth/LogIn.js
@@ -13,15 +13,12 @@ export default function LogIn() {
   const [loading, setLoading] = useState(false);
   const history = useHistory();
 
-  async function handleSubmit(event) {
-    // stops the form from submitting
-    event.preventDefault();
-
+  async function handleSubmit(event, email, pass) {
     try {
       // neccesary cuz user may try again and if different error, previous error should go away
       setLoading(true);
       setError("");
-      await login(emailRef.current.value, passwordRef.current.value);
+      await login(email, pass);
       history.push("/");
     } catch {
       setError("Username or password is incorrect");
@@ -52,8 +49,29 @@ export default function LogIn() {
               className="form-input"
             />
           </FormGroup>
-          <button disabled={loading} onClick={handleSubmit} className="button">
+          <button
+            disabled={loading}
+            onClick={(e) => {
+              // stops the form from submitting
+              e.preventDefault();
+              handleSubmit(
+                e,
+                emailRef.current.value,
+                passwordRef.current.value
+              );
+            }}
+            className="button"
+          >
             Log In
+          </button>
+          <button
+            disabled={loading}
+            className="button"
+            onClick={(e) => {
+              handleSubmit(e, "guest@email.com", "youtdskjdfiu3333");
+            }}
+          >
+            Login as guest
           </button>
         </form>
         {error && <Message type="error" message={error} />}
