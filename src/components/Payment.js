b3def77b6e285a2ff41b44227cf23dd995d12877
Arnav Shekaran <arnard76@gmail.com>
2022-02-22 21:00:27 +1300

    Showing inactive payments
    also sorting the active payments by frequency ... somehow lol

--- src/components/Payment.js
+++ src/components/Payment.js
@@ -1,12 +1,17 @@
 import React from "react";
 
-export default function Payment({ payment, onClick }) {
+// active payment is a payment that is in the filteredPayments
+export default function Payment({ payment, onClick, active }) {
   const { name, amount, frequency } = payment;
   return (
     <>
       <div
         onClick={onClick}
-        className="relative z-10 flex justify-evenly p-2 my-2 bg-blue-50 dark:bg-gray-900 dark:text-white rounded-xl border-blue-400 border-2 "
+        className={`relative z-10 flex justify-evenly p-2 my-2   rounded-xl ${
+          active
+            ? `border-blue-400 bg-blue-50 dark:bg-gray-900 dark:text-white`
+            : `border-blue-200 bg-gray-400 dark:bg-gray-900 dark:text-white `
+        }  border-2 `}
         style={{ cursor: "pointer" }}
       >
         <p className="flex-1 ">${amount}</p>
--- src/components/Payments.js
+++ src/components/Payments.js
@@ -7,7 +7,8 @@ import EditPaymentForm from "./Forms/EditPaymentForm";
 export default function Payments() {
   const [showingPayment, setShowingPayment] = useState({ id: null });
   const [editingPayment, setEditingPayment] = useState({ id: null });
-  let { userDetails, filteredPayments: payments, annualTotal } = usePayments();
+  let { userDetails, filteredPayments, payments, periodTotal, calcOccurances } =
+    usePayments();
 
   function handleShowPayment(payment) {
     if (showingPayment.id === payment.id) {
@@ -57,12 +58,64 @@ export default function Payments() {
 
   return (
     <>
-      <div className="flex flex-1 ">
-        <div className="relative  flex-1 p-4 h-screen overflow-y-auto bg-white dark:bg-gray-900 z-10">
-          <div className="text-gray-900 dark:text-white  flex flex-col justify-center z-10 relative">
+      <div className="flex flex-1 flex-col h-screen relative">
+        <div className="  flex-1 p-4 overflow-y-auto bg-white dark:bg-gray-900 z-10">
+          <div className="text-gray-900 dark:text-white  flex flex-col justify-center relative">
             <h1 className="relative z-10 text-xl text-center text-gray-900 dark:text-white font-bold">
               What does your financial goal look like?
             </h1>
+          </div>
+          <div>
+            {filteredPayments
+              .sort((a, b) => {
+                let aFreq = a.frequency
+                  ? calcOccurances(
+                      a.frequency,
+                      a.start,
+                      a.end ? a.end : userDetails["period end date"]
+                    )
+                  : 1;
+                let bFreq = b.frequency
+                  ? calcOccurances(
+                      b.frequency,
+                      b.start,
+                      b.end ? b.end : userDetails["period end date"]
+                    )
+                  : 1;
+                return bFreq - aFreq;
+              })
+              .map((payment) => {
+                return (
+                  <Payment
+                    onClick={() => handleShowPayment(payment, true)}
+                    key={payment.id}
+                    payment={payment}
+                    active
+                  />
+                );
+              })}
+
+            {payments
+              .filter((payment) => !filteredPayments.includes(payment))
+              .map((payment) => {
+                return (
+                  <Payment
+                    onClick={() => handleShowPayment(payment, false)}
+                    key={payment.id}
+                    payment={payment}
+                    active={false}
+                  />
+                );
+              })}
+          </div>
+          <div
+            onClick={(e) => {
+              handleShowPayment(showingPayment);
+              handleEditPayment(editingPayment);
+            }}
+            className="  absolute top-0 bottom-0 right-0 left-0 z-0"
+          ></div>
+        </div>
 
             {/* QUICK PAYMENTS ANALYSIS */}
             <div className="flex justify-around w-1/2 m-auto">
